adf23114b31b4326caa12d572428b41baefee1c0
kivv00ng <rldnd2637@naver.com>
2022-10-20 12:11:56 +0900

    Gatsby

---
+++ README.md
@@ -0,0 +1,1 @@
+Powered by **[gatsby-starter-hoodie](https://github.com/devHudi/gatsby-starter-hoodie)**
---
+++ blog-config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  title: "kivv00ng",
+  description: "기록하는 공간입니다.",
+  author: "kivv00ng",
+  siteUrl: "https://kivv00ng.io",
+  links: {
+    github: "https://github.com/kivv00ng",
+    //linkedIn: "https://linkedin.com",
+    //email: "mailto:rldnd2637@naver.com",
+    etc: "https://www.notion.so/ko-kr",
+  },
+  utterances: {
+    repo: "kivv00ng/kivv00ng.io",
+    type: "pathname",
+  },
+}
+//https://github.com/devHudi/gatsby-starter-hoodie/blob/develop/blog-config.js
